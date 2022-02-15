eb2f088482aed6d23937724414b840f9cdab83c3
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-02-15 15:40:12 -0300

    Criei desafio02

---
+++ scripts-python/desafio02.py
@@ -0,0 +1,6 @@
+print('====== DESAFIO 02 ======')
+print('Data de Natalidade')
+dia = input('Dia =')
+mes = input('Mês =')
+ano = input('Ano =')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
