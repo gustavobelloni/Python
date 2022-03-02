5749c68190a22aa2dd61bf4c9bca59d4a8d99019
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-03-02 12:49:31 -0300

    Aprendendo mais sobre python

---
+++ aulas/aula018.py
@@ -0,0 +1,25 @@
+''' teste = list()
+teste.append('Gustavo')
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera) '''
+galera = list()
+dado = list()
+totmai = totmen = 0
+for c in range(0, 5):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+        totmai += 1
+    else:
+        print(f'{p[0]} é menor de idade.')
+        totmen += 1
+print(f'Temos {totmai} maiores e {totmen} menores de idade.')
