b8634936e4834fe7b9aba08472aca14b5ee0e0ba
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-03-17 18:19:50 -0300

    Melhorando o jogo de adivinhação

--- alurapython/jogos/adivinhacao.py
+++ alurapython/jogos/adivinhacao.py
@@ -1,23 +1,26 @@
+from random import randint
+
 print('-' * 33)
 print('Bem vindo ao jogo de adivinhação!')
 print('-' * 33)
 
-numero_secreto = 42
-
-chute = int(input('Digite o seu número: '))
-
-print(f'Você digitou {chute}')
-
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if acertou:
-    print('Você acertou!')
-else:
-    if maior:
-        print('Você errou, o seu chute foi maior do que valor correto!')
-    elif menor:
-        print('Você errou, seu chute foi menor do que valor correto!')
-
+numero_secreto = randint(0, 100)
+tentativas = 3
+while tentativas > 0:
+    print(f'Você tem {tentativas} tentativas')
+    chute = int(input('Digite um número de 0 a 100: '))
+    print(f'Você digitou {chute}')
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    if acertou:
+        print('Você acertou!')
+        break
+    else:
+        if maior:
+            print('Você errou, o seu chute foi maior do que valor correto!')
+        elif menor:
+            print('Você errou, seu chute foi menor do que valor correto!')
+    tentativas -= 1
+print(f'O número era {numero_secreto}')
 print('Fim do jogo!')
