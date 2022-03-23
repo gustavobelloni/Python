a10cdb7e6c329ceb743a4c659a796e54e4c46fdd
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-03-23 18:47:50 -0300

    Melhorando o jogo da forca

--- alurapython/jogos/forca.py
+++ alurapython/jogos/forca.py
@@ -4,12 +4,24 @@ def jogar():
     print('-' * 27)
 
     palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
 
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     while not enforcou and not acertou:
-        print('Jogando...')
+
+        chute = str(input('Qual letra? ')).lower().strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra:
+                letras_acertadas[index] = letra
+            index += 1
+
+        print(letras_acertadas)
 
     print('Fim do Jogo!')
 
