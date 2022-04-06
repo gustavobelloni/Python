37a806cb695c83a49c9229458680393f5b4396b0
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-04-06 15:46:44 -0300

    Criando uma classe sobre conta no python

--- oo/conta.py
+++ oo/conta.py
@@ -3,10 +3,24 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print(f'Construindo objeto... {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'Saldo de {self.__saldo} reais de {self.__titular} ')
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
 
 
-Conta()
+conta = Conta(123, 'Gustavo', 55.5, 1000.0)
+conta2 = Conta(321, 'Nico', 70.0, 1000.0)
