6f28d6ce13d7fd99c03bf1640079272ac3364411
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-04-07 17:30:38 -0300

    Finalizando a introdução a O.O.

--- oo/conta.py
+++ oo/conta.py
@@ -14,13 +14,40 @@ class Conta:
     def depositar(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def sacar(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print(f'O valor {valor} passou do limite')
 
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
 
+    @property
+    def saldo(self):
+        return self.__saldo
 
-conta = Conta(123, 'Gustavo', 55.5, 1000.0)
-conta2 = Conta(321, 'Nico', 70.0, 1000.0)
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
