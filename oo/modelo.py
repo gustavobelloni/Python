ac1782db598aac4623fba1eb3f34a5a1ebdc102b
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-04-27 19:46:54 -0300

    terminando a criação de playlist e resolvendo exercicio

--- oo/modelo.py
+++ oo/modelo.py
@@ -38,18 +38,39 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
 atlanta = Serie('atlanta', 2018, 2)
+branquelas = Filme('As branquelas', 2010, 100)
+cavaleiro_da_lua = Serie('Cavaleiro da Lua', 2022, 1)
+
+vingadores.dar_like()
+vingadores.dar_like()
+branquelas.dar_like()
+cavaleiro_da_lua.dar_like()
+cavaleiro_da_lua.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
+filmes_series = [vingadores, atlanta, cavaleiro_da_lua, branquelas]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_series)
 
-filmes_series = [vingadores, atlanta]
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
 
-for programa in filmes_series:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
-
-
-
-
