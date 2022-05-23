aad6ac8c0a9aa916c475dcb675d75c756a1aa2c6
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-05-23 13:48:16 -0300

    atualização

--- alurapython/ioarquivos/principal.py
+++ alurapython/ioarquivos/principal.py
@@ -1,8 +1,8 @@
-try:
+'''try:
     with open('dados/contatos.csv', encoding='UTF-8') as arquivo_contatos:
         for linha in arquivo_contatos:
                 print(linha, end='')
 except FileNotFoundError:
     print('Arquino não encontrado.')
 except PermissionError:
-    print('Sem permissão de escrita.')
+    print('Sem permissão de escrita.')'''
