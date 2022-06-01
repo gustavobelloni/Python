156a99d10f460f0fcaea18a87d35d4b0e3dba493
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-06-01 10:38:28 -0300

    aprendendo sobre conversões

--- alurapython/ioarquivos/principal.py
+++ alurapython/ioarquivos/principal.py
@@ -1,8 +1,18 @@
-'''try:
-    with open('dados/contatos.csv', encoding='UTF-8') as arquivo_contatos:
-        for linha in arquivo_contatos:
-                print(linha, end='')
+import contatos_uteis
+
+try:
+    # contatos = contatos_uteis.csv_para_contatos('dados/contatos.csv', encoding='UTF-8')
+    # contatos_uteis.contatos_para_pickle(contatos, 'dados/contatos.pickle')
+
+    # contatos = contatos_uteis.pickle_para_contatos('dados/contatos.pickle')
+    # contatos_uteis.contatos_para_json(contatos, 'dados/contatos.json')
+
+    contatos = contatos_uteis.json_para_contatos( 'dados/contatos.json')
+
+
+    for contato in contatos:
+        print(f'{contato.id} - {contato.nome} - {contato.email}')
 except FileNotFoundError:
     print('Arquino não encontrado.')
 except PermissionError:
-    print('Sem permissão de escrita.')'''
+    print('Sem permissão de escrita.')
