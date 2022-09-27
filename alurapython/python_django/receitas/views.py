ad00b0ea6263f038571873b053fcb29d5d563b66
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-09-27 19:13:00 -0300

    Aprendendo mais sobre Django

--- alurapython/python_django/receitas/views.py
+++ alurapython/python_django/receitas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
 # Create your views here.
