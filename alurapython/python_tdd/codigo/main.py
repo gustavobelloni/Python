4dc3a9fb3f5985d3aef6b025b92f4ac389ed4669
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-10-26 19:43:24 -0300

    Aprendendo sobre testes automatizados, especificamente testes unitários

---
+++ alurapython/python_tdd/codigo/main.py
@@ -0,0 +1,15 @@
+from codigo.bytebank import Funcionario
+
+
+def teste_idade():
+    funcionario_teste = Funcionario('Teste', '13/03/2000', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+    funcionario_teste1 = Funcionario('Teste', '13/03/1999', 1111)
+    print(f'Teste = {funcionario_teste1.idade()}')
+
+    funcionario_teste2 = Funcionario('Teste', '16/04/1998', 1111)
+    print(f'Teste = {funcionario_teste2.idade()}')
+
+
+teste_idade()
